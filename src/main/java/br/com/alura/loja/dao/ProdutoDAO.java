a50778df0e37b25b305b6ae97c3bf542f17b72b1
JotaPaiva <jonataspaiva2002@hotmail.com>
2023-06-03 00:04:01 -0300

    Persist, merge, remove and find working.

--- src/main/java/br/com/alura/loja/dao/ProdutoDAO.java
+++ src/main/java/br/com/alura/loja/dao/ProdutoDAO.java
@@ -3,6 +3,8 @@ package br.com.alura.loja.dao;
 import br.com.alura.loja.modelo.Produto;
 
 import javax.persistence.EntityManager;
+import java.math.BigDecimal;
+import java.util.List;
 
 public class ProdutoDAO {
 
@@ -22,6 +24,37 @@ public class ProdutoDAO {
 
     public void remover(Produto produto) {
         this.em.remove(produto);
+    }
+
+    public Produto buscarPorId(Long id) {
+        return em.find(Produto.class, id);
+    }
+
+    public List<Produto> buscarTodos() {
+        String jpql = "SELECT p FROM Produto p";
+        return em.createQuery(jpql, Produto.class)
+                .getResultList();
+    }
+
+    public List<Produto> buscarPorNomeDoProduto(String nomeDoProduto) {
+        String jpql = "SELECT p FROM Produto p WHERE p.nome = ?1";
+        return em.createQuery(jpql, Produto.class)
+                .setParameter(1,nomeDoProduto)
+                .getResultList();
+    }
+
+    public List<Produto> buscarPorNomeDaCategoria(String nomeDaCategoria) {
+        String jpql = "SELECT p FROM Produto p WHERE p.categoria.nome = ?1";
+        return em.createQuery(jpql, Produto.class)
+                .setParameter(1,nomeDaCategoria)
+                .getResultList();
+    }
+
+    public BigDecimal buscarPrecoDoProduto(String nomeDoProduto) {
+        String jpql = "SELECT p.preco FROM Produto p WHERE p.nome = ?1";
+        return em.createQuery(jpql, BigDecimal.class)
+                .setParameter(1,nomeDoProduto)
+                .getSingleResult();
     }
 
 }
--- src/main/java/br/com/alura/loja/testes/CadastroDeProduto.java
+++ src/main/java/br/com/alura/loja/testes/CadastroDeProduto.java
@@ -7,13 +7,30 @@ import br.com.alura.loja.modelo.Produto;
 import br.com.alura.loja.util.JPAUtil;
 
 import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
 import java.math.BigDecimal;
+import java.util.List;
 
 public class CadastroDeProduto {
 
     public static void main(String[] args) {
+
+        cadastrarProduto();
+
+        EntityManager em = JPAUtil.getEntityManager();
+        ProdutoDAO produtoDAO = new ProdutoDAO(em);
+
+        Produto p = produtoDAO.buscarPorId(1l);
+        System.out.println(p.getPreco());
+
+        List<Produto> todos = produtoDAO.buscarPorNomeDaCategoria("CELULARES");
+        todos.forEach(p2 -> System.out.println(p2.getNome()));
+
+        BigDecimal precoDoProduto = produtoDAO.buscarPrecoDoProduto("Samsung Galaxy SE");
+        System.out.println(precoDoProduto);
+
+    }
+
+    private static void cadastrarProduto() {
 
         Categoria celulares = new Categoria("CELULARES");
         Produto celular = new Produto("Samsung Galaxy SE","Cor: Azul, Armazenamento: 128GB, Memória RAM: 6GB",new BigDecimal(2000), celulares);
@@ -26,7 +43,6 @@ public class CadastroDeProduto {
 
         categoriaDAO.cadastrar(celulares);
         produtoDAO.cadastrar(celular);
-        produtoDAO.remover(celular);
         em.getTransaction().commit();
         em.close();
 
